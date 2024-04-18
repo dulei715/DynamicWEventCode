067a3e6a5fbf8f012384284ad35ba2e0af79804b
LeileiDu <dulei715@126.com>
2024-04-18 17:35:19 +0800

    save

--- src/main/java/ecnu/dll/schemes/main_scheme/metric/MechanismError.java
+++ src/main/java/ecnu/dll/schemes/main_scheme/metric/MechanismError.java
@@ -14,6 +14,44 @@ public class MechanismError {
         this.biasError = biasError;
     }
 
+    public static Double getCountError(Integer itemSize, TreeMap<Double, Double> epsilonRatioMap, Double epsilonTheta) {
+        Double totalRatio = MapUtils.getValueSum(epsilonRatioMap);
+        if (Math.abs(totalRatio - 1) > Math.pow(10, -6)) {
+            throw new RuntimeException("The sum of epsilonRatioMap's value is not equal to 1!");
+        }
+        Double result = 0D, countVar = 0D, tempEpsilon, tempRatio, tempProbability, tempValue;
+        for (Map.Entry<Double, Double> entry : epsilonRatioMap.entrySet()) {
+            tempEpsilon = entry.getKey();
+            if (tempEpsilon >= epsilonTheta) {
+                break;
+            }
+            tempProbability = (Math.exp(tempEpsilon) - 1) / (Math.exp(epsilonTheta) - 1);
+            tempRatio = entry.getValue();
+            tempValue = itemSize * tempRatio * (1 - tempProbability);
+            countVar += tempValue * tempProbability;
+        }
+        return  countVar;
+    }
+
+    public static Double getBiasError(Integer itemSize, TreeMap<Double, Double> epsilonRatioMap, Double epsilonTheta) {
+        Double totalRatio = MapUtils.getValueSum(epsilonRatioMap);
+        if (Math.abs(totalRatio - 1) > Math.pow(10, -6)) {
+            throw new RuntimeException("The sum of epsilonRatioMap's value is not equal to 1!");
+        }
+        Double bias = 0D, tempEpsilon, tempRatio, tempProbability, tempValue;
+        for (Map.Entry<Double, Double> entry : epsilonRatioMap.entrySet()) {
+            tempEpsilon = entry.getKey();
+            if (tempEpsilon >= epsilonTheta) {
+                break;
+            }
+            tempProbability = (Math.exp(tempEpsilon) - 1) / (Math.exp(epsilonTheta) - 1);
+            tempRatio = entry.getValue();
+            tempValue = itemSize * tempRatio * (1 - tempProbability);
+            bias += tempValue;
+        }
+        return  bias * bias;
+    }
+
     public static Double getSampleError(Integer itemSize, TreeMap<Double, Double> epsilonRatioMap, Double epsilonTheta) {
         Double totalRatio = MapUtils.getValueSum(epsilonRatioMap);
         if (Math.abs(totalRatio - 1) > Math.pow(10, -6)) {
--- src/test/java/basic_test/MechanismErrorTest.java
+++ src/test/java/basic_test/MechanismErrorTest.java
@@ -30,13 +30,81 @@ public class MechanismErrorTest {
         }
 
         Double[] errorArray = new Double[size], sampleErrorArray = new Double[size], dpErrorArray = new Double[size];
+        Double[] countError = new Double[size], biasError = new Double[size];
         for (int i = 0; i < size; i++) {
+            countError[i] = MechanismError.getCountError(1000, map, epsilonArray[i]);
+            biasError[i] = MechanismError.getBiasError(1000, map, epsilonArray[i]);
             sampleErrorArray[i] = MechanismError.getSampleError(1000, map, epsilonArray[i]);
             dpErrorArray[i] = MechanismError.getDPError(epsilonArray[i]);
             errorArray[i] = sampleErrorArray[i] + dpErrorArray[i];
         }
+        MyPrint.showArray(countError, "; ");
+        MyPrint.showArray(biasError, "; ");
         MyPrint.showArray(sampleErrorArray, "; ");
         MyPrint.showArray(dpErrorArray, "; ");
         MyPrint.showArray(errorArray, "; ");
+
+        double minError = Double.MAX_VALUE;
+        double epsilon = -1, ratio = -1;
+        for (int i = 0; i < errorArray.length; i++) {
+//            if (errorArray[i] < minError) {
+//                minError = errorArray[i];
+//            if (sampleErrorArray[i] < minError) {
+//                minError = sampleErrorArray[i];
+            if (countError[i] < minError) {
+                minError = countError[i];
+                epsilon = epsilonArray[i];
+                ratio = randomDoubleArray[i];
+            }
+        }
+        System.out.printf("epsilon: %f, ration: %f, error: %f\n", epsilon, ratio, minError);
+    }
+
+    @Test
+    public void fun2() {
+        double[] epsilonArray = new double[]{0.1, 0.2, 0.3, 3, 3.1, 10, 10.5};
+        MyPrint.showArray(epsilonArray, "; ");
+        int size = epsilonArray.length;
+        Double[] randomDoubleArray = RandomUtil.getRandomDoubleArray(0D, 1D, size);
+//        MyPrint.showArray(randomDoubleArray, "; ");
+        Double sum = BasicArrayUtil.getSum(randomDoubleArray);
+//        System.out.println(sum);
+        BasicArrayUtil.linearTransform(randomDoubleArray, 1.0/sum, 0D);
+//        MyPrint.showArray(randomDoubleArray, "; ");
+
+        TreeMap<Double, Double> map = new TreeMap<>();
+        for (int i = 0; i < size; i++) {
+            map.put(epsilonArray[i], randomDoubleArray[i]);
+        }
+
+        Double[] errorArray = new Double[size], sampleErrorArray = new Double[size], dpErrorArray = new Double[size];
+        Double[] countError = new Double[size], biasError = new Double[size];
+        for (int i = 0; i < size; i++) {
+            countError[i] = MechanismError.getCountError(1000, map, epsilonArray[i]);
+            biasError[i] = MechanismError.getBiasError(1000, map, epsilonArray[i]);
+            sampleErrorArray[i] = MechanismError.getSampleError(1000, map, epsilonArray[i]);
+            dpErrorArray[i] = MechanismError.getDPError(epsilonArray[i]);
+            errorArray[i] = sampleErrorArray[i] + dpErrorArray[i];
+        }
+        MyPrint.showArray(countError, "; ");
+        MyPrint.showArray(biasError, "; ");
+        MyPrint.showArray(sampleErrorArray, "; ");
+        MyPrint.showArray(dpErrorArray, "; ");
+        MyPrint.showArray(errorArray, "; ");
+
+        double minError = Double.MAX_VALUE;
+        double epsilon = -1, ratio = -1;
+        for (int i = 0; i < errorArray.length; i++) {
+//            if (errorArray[i] < minError) {
+//                minError = errorArray[i];
+//            if (sampleErrorArray[i] < minError) {
+//                minError = sampleErrorArray[i];
+            if (countError[i] < minError) {
+                minError = countError[i];
+                epsilon = epsilonArray[i];
+                ratio = randomDoubleArray[i];
+            }
+        }
+        System.out.printf("epsilon: %f, ration: %f, error: %f\n", epsilon, ratio, minError);
     }
 }
