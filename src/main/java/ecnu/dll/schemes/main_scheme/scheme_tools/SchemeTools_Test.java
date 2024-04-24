11ddfb680aa8fd041254daf64120387e0474a34b
pisces <dulei715@126.com>
2024-04-24 12:24:39 +0800

    save

---
+++ src/main/java/ecnu/dll/schemes/main_scheme/scheme_tools/SchemeTools_Test.java
@@ -0,0 +1,7 @@
+package ecnu.dll.schemes.main_scheme.scheme_tools;
+
+public class SchemeTools_Test {
+    public static Double getDissimilarity(Double[] budgetArray) {
+        return null;
+    }
+}
