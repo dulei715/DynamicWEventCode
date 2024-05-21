259867e36dc8990abee01a840e093f849e3eeb25
LeileiDu <dulei715@126.com>
2024-05-21 13:32:07 +0800

    Saved by Leilei through a script

--- src/test/java/basic_test/DatasetTest.java
+++ src/test/java/basic_test/DatasetTest.java
@@ -13,4 +13,23 @@ public class DatasetTest {
         Double[] result = DataSetHandler.getLinearNormalSequence(p0, standardVariance, size);
         MyPrint.showArray(result);
     }
+
+    @Test
+    public void fun2() {
+        Double amplitude = 0.05;
+        Double angularVelocity = 0.01;
+        Double initialY = 0.075;
+        int size = 100;
+        Double[] result = DataSetHandler.getSinSequence(amplitude, angularVelocity, initialY, size);
+        MyPrint.showArray(result);
+    }
+
+    @Test
+    public void fun3() {
+        Double valueA = 0.25;
+        Double valueB = 0.01;
+        int size = 100;
+        Double[] result = DataSetHandler.getLogSequence(valueA, valueB, size);
+        MyPrint.showArray(result);
+    }
 }
