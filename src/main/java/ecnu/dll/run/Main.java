4f6222a7870965a92c8568a4eed87e2959b7ad44
LeileiDu <dulei715@126.com>
2024-06-09 11:26:04 +0800

    Saved by Leilei through a script

--- src/main/java/ecnu/dll/run/Main.java
+++ src/main/java/ecnu/dll/run/Main.java
@@ -1,11 +1,24 @@
 package ecnu.dll.run;
 
 public class Main {
-    public static void main0(String[] args) {
-        System.out.println("Hello Main");
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hello Dynamic WEvent!");
-    }
+    /**
+     * 自变量指标
+     *      1.
+     * 因变量指标：
+     *      1. Runing time
+     *      2. MRE, MAE
+     *
+     * 对比方案
+     *      1. 基本方案
+     *          (1) BD (统一为最小的 privacy buget 和最大的 window size)
+     *          (2) BA (统一为最小的 privacy buget 和最大的 window size)
+     *      2. 额外方案
+     *          (1) 针对 PBD 和 PBA
+     *          (2) 针对 PDBD 和 PDBA
+     *      3. 本方案
+     *          (1) PBD (每个用户各自统一为最小的 privacy budget 和最大的 window size)
+     *          (2) PBA (每个用户各自统一为最小的 privacy budget 和最大的 window size)
+     *          (3) PDBD
+     *          (4) PDBA
+     */
 }
