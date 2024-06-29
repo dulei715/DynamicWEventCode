b2b526cad4d767436bf9bdf818d821dbc0db8f36
LeileiDu <dulei715@126.com>
2024-06-29 21:29:06 +0800

    Saved by Leilei through a script

--- src/test/java/important_test/DatasetCheckInTest.java
+++ src/test/java/important_test/DatasetCheckInTest.java
@@ -1,5 +1,6 @@
 package important_test;
 
+import cn.edu.dll.statistic.StatisticTool;
 import ecnu.dll._config.Constant;
 import ecnu.dll._config.TestData;
 import cn.edu.dll.basic.StringUtil;
@@ -7,6 +8,7 @@ import cn.edu.dll.constant_values.ConstantValues;
 import cn.edu.dll.io.print.MyPrint;
 import cn.edu.dll.io.read.BasicRead;
 import ecnu.dll.dataset.real.datasetB.basic_struct.CityBean;
+import ecnu.dll.dataset.real.datasetB.handled_struct.CheckInSimplifiedBean;
 import ecnu.dll.dataset.real.datasetB.spetial_tools.CheckInStringTool;
 import org.junit.Test;
 
@@ -54,6 +56,7 @@ public class DatasetCheckInTest {
         System.out.println(cityBeanMap.size());
     }
 
+    // 测试各个Country在check in 数据中各自关联多少个数据
     @Test
     public void testCountryStatistic() {
         String filterFileDirectory = StringUtil.join(ConstantValues.FILE_SPLIT, Constant.checkInFilePath, "join");
@@ -79,6 +82,101 @@ public class DatasetCheckInTest {
 
     }
 
+    @Test
+    public void testTime() {
+        String directoryPath = Constant.checkInFilePath;
+        String handledDirectoryPath = StringUtil.join(ConstantValues.FILE_SPLIT, directoryPath, "join");
+        File directoryFile = new File(handledDirectoryPath);
+        File[] files = directoryFile.listFiles();
+        BasicRead basicRead = new BasicRead(",");
+        List<String> lineDataList;
+        CheckInSimplifiedBean tempBean;
+        Long maxTimeStamp = 0L, minTimeStamp = Long.MAX_VALUE, tempTimeStamp;
+        int k = 0;
+        for (File file : files) {
+            basicRead.startReading(file.getAbsolutePath());
+            lineDataList = basicRead.readAllWithoutLineNumberRecordInFile();
+            for (String line : lineDataList) {
+                ++k;
+                tempBean = CheckInSimplifiedBean.toBean(basicRead.split(line));
+                tempTimeStamp = tempBean.getCheckInTimeStamp();
+                minTimeStamp = Math.min(minTimeStamp, tempTimeStamp);
+                maxTimeStamp = Math.max(maxTimeStamp, tempTimeStamp);
+            }
+            basicRead.endReading();
+        }
+        System.out.println(k);
+        Date minDate = new Date(minTimeStamp);
+        Date maxDate = new Date(maxTimeStamp);
+        System.out.println(minDate);
+        System.out.println(maxDate);
+        System.out.println(minTimeStamp);
+        System.out.println(maxTimeStamp);
+        System.out.println((maxTimeStamp-minTimeStamp)/1000.0/60/10);
+    }
+
+    @Test
+    public void testTimeStampDifference() {
+        String directoryPath = Constant.checkInFilePath;
+        String handledDirectoryPath = StringUtil.join(ConstantValues.FILE_SPLIT, directoryPath, "join");
+        File directoryFile = new File(handledDirectoryPath);
+        File[] files = directoryFile.listFiles();
+        BasicRead basicRead = new BasicRead(",");
+        List<String> lineDataList;
+        CheckInSimplifiedBean tempBean;
+        TreeMap<Long, Integer> differenceMap = new TreeMap<>();
+        Long tempDifference, oldTimeStamp, newTimeStamp = 0L;
+        int k = 0;
+        for (File file : files) {
+            basicRead.startReading(file.getAbsolutePath());
+            lineDataList = basicRead.readAllWithoutLineNumberRecordInFile();
+            for (String line : lineDataList) {
+                ++k;
+                oldTimeStamp = newTimeStamp;
+                tempBean = CheckInSimplifiedBean.toBean(basicRead.split(line));
+                newTimeStamp = tempBean.getCheckInTimeStamp();
+                tempDifference = newTimeStamp - oldTimeStamp;
+                StatisticTool.addElement(differenceMap, tempDifference);
+            }
+            basicRead.endReading();
+        }
+        System.out.println(k);
+        MyPrint.showMap(differenceMap);
+    }
+
+
+    @Test
+    public void testEachUserCheckInInterval() {
+        String directoryPath = Constant.checkInFilePath;
+        String handledDirectoryPath = StringUtil.join(ConstantValues.FILE_SPLIT, directoryPath, "join");
+        File directoryFile = new File(handledDirectoryPath);
+        File[] files = directoryFile.listFiles();
+        BasicRead basicRead = new BasicRead(",");
+        List<String> lineDataList;
+        CheckInSimplifiedBean tempBean;
+        TreeMap<Long, Integer> differenceMap = new TreeMap<>();
+        Long tempDifference, oldTimeStamp, newTimeStamp;
+        Map<Integer, Long> currentMap = new TreeMap<>();
+        Map<Integer, Map<Integer, Integer>> differMap = new TreeMap<>();
+        Integer tempUserID;
+        int k = 0;
+        for (File file : files) {
+            basicRead.startReading(file.getAbsolutePath());
+            lineDataList = basicRead.readAllWithoutLineNumberRecordInFile();
+            for (String line : lineDataList) {
+                ++k;
+                tempBean = CheckInSimplifiedBean.toBean(basicRead.split(line));
+                tempUserID = tempBean.getUserID();
+                newTimeStamp = tempBean.getCheckInTimeStamp();
+                oldTimeStamp = currentMap.getOrDefault(tempUserID, 0L);
+                //todo: 添加tag
+            }
+            basicRead.endReading();
+        }
+        System.out.println(k);
+        MyPrint.showMap(differenceMap);
+
+    }
 
 
 
