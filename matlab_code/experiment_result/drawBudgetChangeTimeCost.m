a33eac4409bbd37bbe7754c4337f8b1b2a8f1acf
Leilei Du <dulei715@126.com>
2024-12-15 23:35:53 +0800

    Saved by Leilei through a script

---
+++ matlab_code/experiment_result/drawBudgetChangeTimeCost.m
@@ -0,0 +1,13 @@
+function y = drawBudgetChangeTimeCost(input_basicPath, output_basicPath)
+dataset_dirs = ["1.trajectory_result", "2.check_in_result", "3.tlns_result", "4.sin_result", "5.log_result"];
+output_dataset_names = ["trajectory", "check_in", "tlns", "sin", "log"];
+default_window_size = 120;
+i = 0;
+for dir_name = dataset_dirs
+    i = i + 1;
+    abs_dir = strcat(input_basicPath, dir_name);
+    %outputFileName = strcat(output_basicPath, 'fig/', 'experiment_result/', output_dataset_names(i), "_budget_change.eps");
+    outputFileName = fullfile(char(output_basicPath), 'fig', 'experiment_result', char(strcat(output_dataset_names(i),"_budget_change_running_time.eps")));
+    %drawBudgetChangeForSingleDatset(abs_dir, default_window_size, outputFileName);
+    drawBudgetChangeOnlyForTimeCostSingleDataset(abs_dir, default_window_size, outputFileName);
+end
--- matlab_code/experiment_result/drawWindowSizeChangeOnlyForTimeCostSingleDataset.m
+++ matlab_code/experiment_result/drawWindowSizeChangeOnlyForTimeCostSingleDataset.m
@@ -1,43 +1,59 @@
-function y = drawBudgetChangeOnlyForTimeCostForSingleDatsetExceptDynamic(basic_path, default_window_size, outputFileName)
+function y = drawWindowSizeChangeOnlyForTimeCostSingleDataset(basic_path, default_budget,outputFileName)
 % haha = @utils/list_dir_name;
 % disp(haha);
 dir_names = list_dir_name(basic_path);
-y_bd = zeros(1,5);
-y_ba = zeros(1,5);
-y_pbd = zeros(1,5);
-y_pba = zeros(1,5);
+
+%y_bd = zeros(1,5);
+%y_ba = zeros(1,5);
+%y_pbd = zeros(1,5);
+%y_pba = zeros(1,5);
+
+data = zeros(5,5);  % each line contains x and all methods' y
+
 %y_pdbd = zeros(1,5);
 %y_pdba = zeros(1,5);
-x = zeros(1,5);
+
+%x = zeros(1,5);
 i = 0;
 for temp_name = dir_names
     temp_name = cell2mat(temp_name);
     [temp_budget, temp_window_size] = extractBudgetAndWSizeFromDirName(temp_name);
-    if temp_window_size ~= default_window_size
+    if temp_budget ~= default_budget
         continue;
     end
-    % disp(temp_budget);
     i = i + 1;
-    x(i) = temp_budget;
+    %x(i) = temp_window_size;
+    data(i,1) = temp_window_size;
     data_path = fullfile(char(basic_path), temp_name, 'result.txt');
     temp_table = readtable(data_path);
-    y_bd(i) = log(temp_table(2,8).MRE);
-    y_ba(i) = log(temp_table(3,8).MRE);
-    y_pbd(i) = log(temp_table(4,8).MRE);
-    y_pba(i) = log(temp_table(5,8).MRE);
+
+    %y_bd(i) = log(temp_table(2,8).MRE);
+    %y_ba(i) = log(temp_table(3,8).MRE);
+    %y_pbd(i) = log(temp_table(4,8).MRE);
+    %y_pba(i) = log(temp_table(5,8).MRE);
+    data(i,2) = temp_table(2,4).Var4;
+    data(i,3) = temp_table(3,4).Var4;
+    data(i,4) = temp_table(4,4).Var4;
+    data(i,5) = temp_table(5,4).Var4;
+    
 %    y_pdbd(i) = log(temp_table(6,8).MRE);
 %    y_pdba(i) = log(temp_table(7,8).MRE);
 end
 
+data = sortrows(data);
+x = data(:,1);
+y_bd = data(:,2);
+y_ba = data(:,3);
+y_pbd = data(:,4);
+y_pba = data(:,5);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%xLabelName = "\epsilon";
-xLabelName = "$\mathcal{E}$";
+xLabelName = "$w$";
 % yLabelName = 'MRE';
-yLabelName = "log(AMRE)";
+yLabelName = "running time";
 
-%legend_names = ["BD";"BA";"PBD";"PBA";"PDBD";"PDBA"];
-legend_names = ["BD";"BA";"PBD";"PBA"];
+legend_names = ["BD";"BA";"PBD";"PBA";"PDBD";"PDBA"];
 figure_MarkerSize = 20;
 figure_FontSize = 28;
 figure_FontSize_X = 28;
@@ -46,7 +62,7 @@ figure_FontSize_Y = 28;
 fig = figure;
 hold on;
 
-xlabel(xLabelName, 'Interpreter', 'latex');
+xlabel(xLabelName,'Interpreter', 'latex');
 %yyaxis left
 plot(x, y_bd, 'ks-','LineWidth',2, 'MarkerSize',figure_MarkerSize);
 plot(x, y_ba, 'mo-','LineWidth',2, 'MarkerSize',figure_MarkerSize);
@@ -61,7 +77,6 @@ xlim([round(x(1),1) x(length(x))]);
 set(gca,'XTick',round(x,1));
 
 %figure_FontSize = 18;
-%set(get(gca,'XLabel'),'FontSize',figure_FontSize,'FontName','Times New Roman');
 set(get(gca,'XLabel'),'FontSize',figure_FontSize,'FontName','Times New Roman');
 set(get(gca,'YLabel'),'FontSize',figure_FontSize,'FontName','Times New Roman');
 
@@ -69,18 +84,11 @@ set(gca,'FontName','Times New Roman' ,'FontSize',figure_FontSize);
 %set(findobj('FontSize',10),'FontSize',figure_FontSize);
 
 
-set(get(gca,'XLabel'),'FontSize',figure_FontSize_X,'FontName','Time New Roman');
+set(get(gca,'XLabel'),'FontSize',figure_FontSize_X,'FontName','Times New Roman');
 %set(get(gca,'YLabel'),'FontSize',figure_FontSize_Y,'FontName','Times New Roman');
 
-%h = legend(legend_names(1), legend_names(2), legend_names(3), legend_names(4), legend_names(5), legend_names(6), 'Location','Best');
+%h = legend('SubGeoI_2', 'MDSW','HUEM','DAM','DAMShrink', 'SubGeoI_1', 'RAM','Location','Best');
 h = legend(legend_names(1), legend_names(2), legend_names(3), legend_names(4), 'Location','Best');
 set(h,'FontName','Times New Roman','FontSize',14,'FontWeight','normal');
 legend('off');
-%saveas(fig,outputFileName,'eps');
-%print(fig, outputFileName, '-depsc2');
-%saveas(fig,outputFileName,'fig');
-
 export_fig(fig , '-pdf' , '-r256' , '-transparent' , outputFileName);
-
-%export_fig(outputFileName, '.eps');
-%print(fig, outputFileName,'-dpdf');
