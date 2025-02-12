973112af860da1d512afcf77416eb4259f1f0f70
LeileiDu <dulei715@126.com>
2025-02-12 14:21:16 +0800

    Saved by Leilei through a script

--- src/main/java/others/signal_test/SignalTest.java
+++ src/main/java/others/signal_test/SignalTest.java
@@ -1,22 +1,22 @@
 package others.signal_test;
 
 import cn.edu.dll.signal.signal_handle.NoTerminalHandler;
-import sun.misc.Signal;
-import sun.misc.SignalHandler;
+//import sun.misc.Signal;
+//import sun.misc.SignalHandler;
 
 public class SignalTest {
     public static void main(String[] args) {
-        SignalHandler signalHandler = new NoTerminalHandler(2);
-        Integer sleepTime = 60000;
-
-        try {
-            Signal signal = new Signal("TERM");
-            Signal.handle(signal, signalHandler);
-            System.out.println("process starts...");
-            Thread.sleep(sleepTime);
-            System.out.println("process ends...");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        SignalHandler signalHandler = new NoTerminalHandler(2);
+//        Integer sleepTime = 60000;
+//
+//        try {
+//            Signal signal = new Signal("TERM");
+//            Signal.handle(signal, signalHandler);
+//            System.out.println("process starts...");
+//            Thread.sleep(sleepTime);
+//            System.out.println("process ends...");
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
