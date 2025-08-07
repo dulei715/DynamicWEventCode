913ca4fcb2276ed5619bbc4d39c6ed04db761a50
dulei715 <dulei715@126.com>
2025-08-07 17:08:28 +0800

    Saved by Leilei through a script

---
+++ src/test/java/tools_usage/ExamplesForMethods.java
@@ -0,0 +1,10 @@
+package tools_usage;
+
+public class ExamplesForMethods {
+    public static void showResult(double[] distinctBudgetArray, double[] budgetCountArray, int maxWindowSize, int[] obfuscatedCount) {
+
+    }
+    public void PLPD() {
+
+    }
+}
