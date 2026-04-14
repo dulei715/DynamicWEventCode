f6a31df4a4b90d3c840925fa3a1bd9f30f31c915
dulei715 <dulei715@126.com>
2026-04-14 16:34:00 +0800

    save

--- src/main/java/ecnu/dll/schemes/compared_scheme/w_event_dp/SPAS.java
+++ src/main/java/ecnu/dll/schemes/compared_scheme/w_event_dp/SPAS.java
@@ -1,7 +1,22 @@
 package ecnu.dll.schemes.compared_scheme.w_event_dp;
 
 import ecnu.dll.schemes._basic_struct.Mechanism;
+import ecnu.dll.struts.stream_data.StreamDataElement;
+import ecnu.dll.struts.stream_data.StreamNoiseCountData;
+
+import java.util.List;
 
 public class SPAS extends Mechanism {
+
+    protected int currentTime;
+    protected Double privacyBudget;
+    protected StreamNoiseCountData lastReleaseNoiseCountMap;
+
+    protected Integer sampleCount;
+
+    public boolean updateNextPublicationResult(List<StreamDataElement<Boolean>> nextDataElementList) {
+        ++this.currentTime;
+
+    }
 
 }
