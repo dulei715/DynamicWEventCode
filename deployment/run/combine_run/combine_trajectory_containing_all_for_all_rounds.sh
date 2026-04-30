0bff6c20edf535a6d122b58b155af552060c6001
dulei715 <dulei715@126.com>
2026-04-30 12:24:23 +0800

    Saved by Mac-LeileiDu

---
+++ deployment/run/combine_run/combine_trajectory_containing_all_for_all_rounds.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+class_name="Trajectory"
+
+#totalMem="-Xms120g"
+#maxMem="-Xmx2400g"
+exp_class_name="Total"
+java ${totalMem} ${maxMem} -cp DynamicWEventCode-1.0-DMDP-jar-with-dependencies.jar ecnu.dll.run.d_total_run._4_added_run._3_5_poster_process_run.${class_name}Containing${exp_class_name}PosterRun
